11d9e7aa2ee699fa247d4767e01fa7465497d9fc
Prashans Dixit <71697179+prishuprograms@users.noreply.github.com>
2020-11-18 09:04:34 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -265,9 +265,9 @@ insta_profile_2 = 'https://www.instagram.com/_.gr.ey._/'
 
 st.sidebar.markdown('About Us 🔍 :  Reach out to 👇🏻')
 if st.sidebar.button("Instagram 🔗 : Prashans Dixit"):
-    st.write(insta_profile)
+    st.sidebar.write(insta_profile)
 
 
 if st.sidebar.button("Instagram 🔗 : Muhammed Rajab"):
 #     webbrowser.open_new(insta_profile_2)
-       st.write(insta_profile_2)
+       st.sidebar.write(insta_profile_2)
